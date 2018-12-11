f5694b7be5e141641bb3cb920a3721e77445c5a5
sameerul97 <sameerul97@gmail.com>
2018-12-11 17:59:52 +0000

    Added detail mobile Phone comp

--- src/app/detail-phone/detail-phone.component.ts
+++ src/app/detail-phone/detail-phone.component.ts
@@ -8,13 +8,21 @@ import { PhonesService } from '../phones.service';
   templateUrl: './detail-phone.component.html',
   styleUrls: ['./detail-phone.component.css']
 })
-export class DetailPhoneComponent implements OnInit {
 
+export class DetailPhoneComponent implements OnInit {
+  public phone = new Phone;
+  public sizeVariant = [];
+  public colorVariant = [];
+  public selectedSizeVariant = "";
+  public selectedColorVariant = "";
+  public phonePrice = "";
+  public phoneAvailability = "";
+  public currentMobileId ;
   constructor(private route: ActivatedRoute,
     private router: Router, private phoneService: PhonesService) { }
-
   ngOnInit() {
     console.log(this.phoneService.currentMobilePhoneId)
+    // this.sizeVariant = [12,213]
     // let id = this.route.snapshot.paramMap.get('mobileId');
     // let id = this.route.snapshot.queryParamMap.get('phone.mobileId');
     // console.log("ID " + id);
@@ -23,13 +31,55 @@ export class DetailPhoneComponent implements OnInit {
     if (this.phoneService.currentMobilePhoneId == undefined) {
       console.log(this.phoneService.currentMobilePhoneId)
       // if (this.phoneService.currentMobilePhoneId.length < 0) {
-        this.router.navigate(['/allPhones'])
+      this.router.navigate(['/home'])
       // }
     }
-    else{
-      console.log("ALL GOOD")
+    else {
+      this.phoneService.getMobilePhone(this.phoneService.currentMobilePhoneId).subscribe(res =>
+        this.storeData(res)
+      );
     }
+  }
+  storeData(res) {
+    // console.log(res.mobileData);
+    console.log(res);
+    var res = res.MobileData;
+    var tempSize = res.sizeVariant;
+    var tempColours = res.colourVariant;
+    this.phonePrice = res.mobilePrice;
+    this.phoneAvailability = res.mobileStock;
+    for (let key in tempSize) {
+      let value = tempSize[key];
+      this.sizeVariant.push(value)
+    }
+    for (let key in tempColours) {
+      let value = tempColours[key];
+      this.colorVariant.push(value);
+    }
+    // console.log(this.colorVariant);
+    // set the size variant into first value by default when loading
+    this.selectedSizeVariant = this.sizeVariant[0];
+    this.selectedColorVariant = this.colorVariant[0];
 
+    this.phone.mobileId = res.mobileId;
+    this.phone.mobileName = res.mobileName;
+    this.phone.mobilePrice = res.mobilePrice;
+    this.phone.imageUrl = res.imageUrl;
+    // console.log(this.phone);
   }
 
+  loadSelectedSize(size){
+    // console.log(size);
+    this.selectedSizeVariant = size;
+  }
+  loadSelectedColor(color){
+    // console.log(size);
+    this.selectedColorVariant = color;
+  }
+}
+export class Phone {
+  mobileId: Number
+  mobileName: String
+  mobilePrice: Number
+  imageUrl: String
 }
--- src/app/phones.service.ts
+++ src/app/phones.service.ts
@@ -15,7 +15,7 @@ export class PhonesService {
   
 
   constructor(private http_Var: HttpClient) { }
-  public authToken : string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfSWQiOiI1YzAwNjFmNWI0NWZlMDBjYTlmYzFjNzEiLCJuYW1lIjoic2FtZWVlcnVsIiwiZW1haWwiOiJzYW1lZXJ1bDk3QGdtYWlsLmNvbTQifSwiaWF0IjoxNTQ0NDgyODIyLCJleHAiOjE1NDQ0ODY0MjIsInN1YiI6InNhbWVlZXJ1bCJ9.7u4Q34USMPl8rR61I-JxUy9qx2KgNtLZojtnzFazxKc"
+  public authToken : string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJfSWQiOiI1YzAwNjFmNWI0NWZlMDBjYTlmYzFjNzEiLCJuYW1lIjoic2FtZWVlcnVsIiwiZW1haWwiOiJzYW1lZXJ1bDk3QGdtYWlsLmNvbTQifSwiaWF0IjoxNTQ0NTQ0MDIwLCJleHAiOjE1NDQ1NDc2MjAsInN1YiI6InNhbWVlZXJ1bCJ9.uTpZat94fR3wwIJQBGuzCH6xQeS8MeqinNF5q_t8yWY"
   public currentMobilePhoneId;
   getAllMobilePhones(): Observable<mobilePhone[]> {
     // this.data = this.http.get<LoginMessage[]>(this._url);
@@ -34,6 +34,16 @@ export class PhonesService {
     let headers = new HttpHeaders().set('Authorization',
       "Bearer "+ this.authToken);
     return this.http_Var.get<mobilePhone[]>("http://localhost:3000/bestSelling", { headers }
+    )
+  }
+
+  getMobilePhone(mobileId): Observable<mobilePhone[]> {
+    // this.data = this.http.get<LoginMessage[]>(this._url);
+    // console.log(this.data.Message)
+    // console.log(this.http.get<LoginMessage>(this._url)); 
+    let headers = new HttpHeaders().set('Authorization',
+      "Bearer "+ this.authToken);
+    return this.http_Var.get<mobilePhone[]>("http://localhost:3000/getPhone/"+mobileId, { headers }
     )
   }
 }
